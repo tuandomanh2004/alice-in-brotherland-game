63cf78dd228c0262b1146e66b709cc2ea65f142e
tuando <tuandomanh2004@gmail.com>
2025-10-24 21:21:18 +0700

    import player model from mixamo, make idle and walk animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator; 
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("running");
+        }
+    }
+}
