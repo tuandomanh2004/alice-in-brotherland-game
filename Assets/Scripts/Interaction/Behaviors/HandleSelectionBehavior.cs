dd40dacd4b0aaa70a75b809dfea807f2393d049f
tuando <tuandomanh2004@gmail.com>
2025-12-06 12:56:22 +0700

    feat: complete the card selection

--- Assets/Scripts/Interaction/Behaviors/HandleSelectionBehavior.cs
+++ Assets/Scripts/Interaction/Behaviors/HandleSelectionBehavior.cs
@@ -1,26 +1,44 @@
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 
 public class HandleSelectionBehavior : MonoBehaviour
 {
-    [SerializeField] CameraSwitcher camSW ; 
-    [SerializeField] CinemachineCamera wallCamera ;
-    [SerializeField] CameraTarget wall ;
-    [SerializeField] Transform camPos ;
+    [SerializeField] private CameraSwitcher camSW;
+    [SerializeField] private CinemachineCamera wallCamera;
+   [SerializeField] private CameraTarget wall;
+    [SerializeField] private Transform camPos;
+    [SerializeField] private OpenTheWall wallPivot;
+    [SerializeField] private float delayAfterTransition;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void HandleClick()
-    { 
-        wallCamera.LookAt = wall.TrackingTarget ;
-        wallCamera.transform.position = camPos.position ; 
+    {
+        SetUpCameraFocus();
+        StartCoroutine(MakeRotation(delayAfterTransition)) ; 
+    }
+    private void SetUpCameraFocus()
+    {
+        wallCamera.LookAt = wall.TrackingTarget;
+        wallCamera.transform.position = camPos.position;
         camSW.SwitchCamera(wallCamera);
+    }
+    // private void MakeRotation()
+    // {
+    //     wallPivot.DownTheWall() ;
+    // }
+    IEnumerator MakeRotation(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        wallPivot.DownTheWall() ;
     }
 }
