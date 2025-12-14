070ad9e5e751460f9674d096886e399723e6db50
tuando <tuandomanh2004@gmail.com>
2025-12-14 15:08:32 +0700

    feat: first try dissolve effect script

---
+++ Assets/Scripts/Interaction/Behaviors/SetDissolveBehavior.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using NUnit.Framework.Internal;
+using UnityEngine;
+
+public class SetDissolveBehavior : MonoBehaviour
+{
+    [SerializeField] private Material dissolveMaterial;
+    [SerializeField] private float dissolveStrength ;
+    [SerializeField] private float duration , startValue = 0f , endValue = 1f ;
+    void Start()
+    {
+        Test() ; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+    public void Test()
+    {
+        StartCoroutine(Lerp()) ;
+    }
+    IEnumerator Lerp()
+    {
+        float timeElapse = 0f ; 
+        while(timeElapse < duration)
+        {
+            dissolveStrength = Mathf.Lerp(startValue,endValue,timeElapse/duration) ;
+            timeElapse += Time.deltaTime ;
+            dissolveMaterial.SetFloat("_DissolveStrength",dissolveStrength) ; 
+            Debug.Log(dissolveStrength) ; 
+            yield return null ; // Đảm bảo mỗi vòng lặp chạy trong 1 frame
+        }
+        dissolveStrength = endValue ;
+        dissolveMaterial.SetFloat("_DissolveStrength",dissolveStrength) ; 
+    }
+}
