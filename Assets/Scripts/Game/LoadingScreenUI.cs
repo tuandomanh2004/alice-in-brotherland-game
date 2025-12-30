94d36ecea070f8b218157d146196d6329c01ed0b
tuando <tuandomanh2004@gmail.com>
2025-12-30 15:13:25 +0700

    feat: scene transition with loading screen

---
+++ Assets/Scripts/Game/LoadingScreenUI.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreenUI : MonoBehaviour
+{
+    [SerializeField] private Slider loadingSlider;
+    [SerializeField] private Image spinner;
+    [SerializeField] private float rotationSpeed ; 
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+       UpdateSpinner() ;  
+    }
+    public void UpdateSlider(float progress)
+    {
+        loadingSlider.value = progress ; 
+    }
+    public void UpdateSpinner()
+    {
+        spinner.transform.eulerAngles -= new UnityEngine.Vector3(0f,0f , Time.deltaTime * rotationSpeed) ;
+    }
+}
--- Assets/Scripts/Game/SceneMNG.cs
+++ Assets/Scripts/Game/SceneMNG.cs
@@ -1,11 +1,14 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneMNG : MonoBehaviour
 {
+    [SerializeField] private GameObject loadingScreenUI;
+    [SerializeField] private float delayBetweenScenes;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,7 +18,25 @@ public class SceneMNG : MonoBehaviour
     }
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName , LoadSceneMode.Single);
+        var uiController = loadingScreenUI.GetComponent<LoadingScreenUI>();
+        loadingScreenUI.SetActive(true);
+        StartCoroutine(LoadSceneAsync(sceneName, uiController));
+    }
+    IEnumerator LoadSceneAsync(string sceneName, LoadingScreenUI screenUI)
+    {
+        AsyncOperation sceneOperation = SceneManager.LoadSceneAsync(sceneName);
+        sceneOperation.allowSceneActivation = false;
+        while (sceneOperation.progress < 0.9f)
+        {
+            //Debug.Log($" Loading Progress : {sceneOperation.progress * 100}%");
+            float lerpProgress = Mathf.Clamp01(sceneOperation.progress / 0.9f); // Chuẩn hóa thanh giá trị load tiến trình trong phạm vi [0;1]
+            screenUI.UpdateSlider(lerpProgress);
+            yield return null;
+        }
+        screenUI.UpdateSlider(1f); // full-fill progress bar
+        yield return new WaitForSeconds(delayBetweenScenes);
+        sceneOperation.allowSceneActivation = true;
+        loadingScreenUI.SetActive(false);
     }
     void OnEnable()
     {
@@ -23,11 +44,11 @@ public class SceneMNG : MonoBehaviour
     }
     void OnDisable()
     {
-        SceneManager.sceneLoaded -= OnSceneLoaded ; 
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
     void OnSceneLoaded(Scene sceneName, LoadSceneMode loadMode)
     {
-        MANAGER.Instance.spawnManager.SpawnPlayer(sceneName.name) ; 
-        MANAGER.Instance.camManager.InitializeCameras() ; 
+        MANAGER.Instance.spawnManager.SpawnPlayer(sceneName.name);
+        MANAGER.Instance.camManager.InitializeCameras();
     }
 }
